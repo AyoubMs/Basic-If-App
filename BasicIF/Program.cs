762554ae11e2a0434222fedab43c41ca3dc3aa5d
AyoubMS <chahayb563@gmail.com>
2023-07-05 12:24:29 +0200

    add app

---
+++ BasicIF/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+
+//bool isComplete = true;
+
+//if (isComplete)
+//    Console.WriteLine("The statement was true.");
+//else
+//{
+//    Console.WriteLine("The statement was false.");
+//    Console.WriteLine("This should also run");
+//}
+
+//Console.WriteLine("End of program!");
+
+Console.Write("What is your first name: ");
+string? firstName = Console.ReadLine();
+
+if (firstName.ToLower() == "ayoub")
+{
+    Console.WriteLine("Hello Mr. Chahir");
+}
+else
+{
+    Console.WriteLine($"Hello {firstName}");
+}
+
+Console.WriteLine("End of program.");
